532f8a18e936dc7e2ed9825c5d485213f760e87c
poporolla <mansur_17@mail.ru>
2019-12-06 08:44:42 +0300

    It's works

---
+++ ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SQLiteApp
+{
+	public class ApplicationContext : DbContext
+	{
+		public ApplicationContext()
+			: base("DefaultConnection")
+		{
+
+		}
+		public DbSet<Phone> Phones { get; set; }
+	}
+}
---
+++ Phone.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SQLiteApp
+{
+	public class Phone : INotifyPropertyChanged
+	{
+		private string title;
+		private string company;
+		private int price;
+		
+		public int Id { get; set; }
+
+		public string Title
+		{
+			get
+			{
+				return title;
+			}
+			set
+			{
+				title = value;
+				OnPropertyChanged("Title");
+			}
+		}
+		public string Company
+		{
+			get
+			{
+				return company;
+			}
+			set
+			{
+				company = value;
+				OnPropertyChanged("Company");
+			}
+		}
+		public int Price
+		{
+			get
+			{
+				return price;
+			}
+			set
+			{
+				price = value;
+				OnPropertyChanged("Price");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName]string prop = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+	}
+}
