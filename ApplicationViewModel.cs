34e1f690bbc972ccac0489c30dbe474eac089d92
poporolla <mansur_17@mail.ru>
2019-12-07 11:10:25 +0300

    Final

---
+++ ApplicationViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Runtime.CompilerServices;
+
+namespace SQLiteApp
+{
+	class ApplicationViewModel : INotifyPropertyChanged
+	{
+		ApplicationContext db;
+		RelayCommand addCommand;
+		RelayCommand editCommand;
+		RelayCommand deleteCommand;
+		IEnumerable<Phone> phones;
+
+		public IEnumerable<Phone> Phones
+		{
+			get
+			{
+				return phones;
+			}
+			set
+			{
+				phones = value;
+				OnPropertyChanged("Phones");
+			}
+		}
+		public ApplicationViewModel()
+		{
+			db = new ApplicationContext();
+			db.Phones.Load();
+			Phones = db.Phones.Local.ToBindingList();
+		}
+		public RelayCommand AddCommand
+		{
+			get
+			{
+				return addCommand ??
+					(addCommand = new RelayCommand(obj =>
+					{
+						PhoneWindow phoneWindow = new PhoneWindow(new Phone());
+						if (phoneWindow.ShowDialog() == true)
+						{
+							Phone phone = phoneWindow.Phone;
+							db.Phones.Add(phone);
+							db.SaveChanges();
+						}
+					}));
+			}
+		}
+		public RelayCommand EditCommand
+		{
+			get
+			{
+				return editCommand ??
+					(editCommand = new RelayCommand(selectedItem =>
+					{
+						if (selectedItem == null)
+						{
+							return;
+						}
+						Phone phone = selectedItem as Phone;
+
+						Phone vm = new Phone()
+						{
+							Id = phone.Id,
+							Title = phone.Title,
+							Company = phone.Company,
+							Price = phone.Price
+						};
+						PhoneWindow phoneWindow = new PhoneWindow(vm);
+
+						if (phoneWindow.ShowDialog() == true)
+						{
+							phone = db.Phones.Find(phoneWindow.Phone.Id);
+							if (phone != null)
+							{
+								phone.Company = phoneWindow.Phone.Company;
+								phone.Title = phoneWindow.Phone.Title;
+								phone.Price = phoneWindow.Phone.Price;
+								db.Entry(phone).State = EntityState.Modified;
+								db.SaveChanges();
+							}
+						}
+					},
+					selectedItem =>
+					{
+						return selectedItem != null;
+					}));
+			}
+		}
+		public RelayCommand DeleteCommand
+		{
+			get
+			{
+				return deleteCommand ??
+					(deleteCommand = new RelayCommand(selectedItem =>
+					{
+						if (selectedItem == null)
+						{
+							return;
+						}
+						Phone phone = selectedItem as Phone;
+						db.Phones.Remove(phone);
+						db.SaveChanges();
+					},
+					selectedItem =>
+					{
+						return selectedItem != null;
+					}));
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName]string property = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+		}
+	}
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,67 +21,10 @@ namespace SQLiteApp
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		ApplicationContext db;
 		public MainWindow()
 		{
 			InitializeComponent();
-
-			db = new ApplicationContext();
-			db.Phones.Load();
-			this.DataContext = db.Phones.Local.ToBindingList();
-		}
-
-		private void Add_Click(object sender, RoutedEventArgs e)
-		{
-			PhoneWindow phoneWindow = new PhoneWindow(new Phone());
-			if (phoneWindow.ShowDialog() == true)
-			{
-				Phone phone = phoneWindow.Phone;
-				db.Phones.Add(phone);
-				db.SaveChanges();
-			}
-		}
-
-		private void Edit_Click(object sender, RoutedEventArgs e)
-		{
-			if (phonesList.SelectedItem == null)
-			{
-				return;
-			}
-
-			Phone phone = phonesList.SelectedItem as Phone;
-
-			PhoneWindow phoneWindow = new PhoneWindow(new Phone
-			{
-				Id = phone.Id,
-				Company = phone.Company,
-				Price = phone.Price,
-				Title = phone.Title
-			});
-
-			if (phoneWindow.ShowDialog() == true)
-			{
-				phone = db.Phones.Find(phoneWindow.Phone.Id);
-				if (phone != null)
-				{
-					phone.Company = phoneWindow.Phone.Company;
-					phone.Price = phoneWindow.Phone.Price;
-					phone.Title = phoneWindow.Phone.Title;
-					db.Entry(phone).State = EntityState.Modified;
-					db.SaveChanges();
-				}
-			}
-		}
-
-		private void Delete_Click(object sender, RoutedEventArgs e)
-		{
-			if (phonesList.SelectedItem == null)
-			{
-				return;
-			}
-			Phone phone = phonesList.SelectedItem as Phone;
-			db.Phones.Remove(phone);
-			db.SaveChanges();
+			this.DataContext = new ApplicationViewModel();
 		}
 	}
 }
